156a945e4e3d0eb987ee22333ccc8a7a796705ba
theparadox7 <theunknownparadox07@gmail.com>
2025-08-06 22:08:48 +0530

    error handling added

--- node_express_API/index.js
+++ node_express_API/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
 const port = 3000;
+const postroutes = require('./routes/posts');  
+dotenv.config();
+
 
 // Middleware to parse JSON bodies
+const app = express();
 app.use(express.json());
 
 // Sample route
@@ -10,4 +14,12 @@ app.get('/',(req,res) =>{
     res.send('Hello World from Express!!!!');
 }).listen(port,() =>{
     console.log(`Server started in port ${port}`);
+});
+
+app.use('/posts',postroutes);
+    
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
