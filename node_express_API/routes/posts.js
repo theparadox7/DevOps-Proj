53bd725f5dd9671b856d8696bb867e655b619a8d
theparadox7 <theunknownparadox07@gmail.com>
2025-08-12 18:42:32 +0530

    Get post by ID and create post

--- node_express_API/routes/posts.js
+++ node_express_API/routes/posts.js
@@ -14,3 +14,26 @@ router.get("/", async (req, res) => {
 });
 
 //get post by id
+router.get("/:id", async (req, res) =>{
+    const id = req.params.id;
+    try {
+        const result = await pool.query("SELECT * FROM posts WHERE id=$1", [id]);
+        if(result.rows.length===0) {
+            return res.status(404).json({error: "Post not found"});
+        }
+        res.json({post_detail: result.rows[0]});
+    }catch (err) {
+        return res.status(500).json({error:err.message});
+    }
+});
+
+//create a post
+router.post("/", async (req, res) => {
+    const {title,content,published = true}=req.body;
+    try {
+        const result = await  pool.query("INSERT INTO posts (title,content,published) VALUES ($1,$2,$3) RETURNING *", [title, content, published]);
+        res.status(201).json({post:result.rows[0]});
+    }catch (err) {
+        return res.status(500).json({error:err.message});
+    }
+});
