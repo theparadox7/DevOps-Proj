8a630fac888cada7fc3bb7b3d0a1ea8d4a5e4d5c
theparadox7 <theunknownparadox07@gmail.com>
2025-08-13 18:33:12 +0530

    Db added

---
+++ node_express_API/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = pool;
