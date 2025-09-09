ca872513029a92ddaabc9e8d351d95cc6567c453
theparadox7 <theunknownparadox07@gmail.com>
2025-09-09 17:44:57 +0530

    Docker compose

--- supplychain-devops/app/inventory/models.py
+++ supplychain-devops/app/inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Inventory(models.Model):
+    item = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.item} ({self.quantity}) - {self.status}"
 # Create your models here.
